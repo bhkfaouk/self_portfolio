2fb40383efa107a7e7595942d0a5179069b950e2
bhkfarouk <faroukbouhaka@gmail.com>
2024-10-24 02:05:16 +0200

    fix gh page problem

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom"; // Import HashRouter
 import App from "./App";
 import "./global.css"; // Ensure this path is correct
 
 const rootElement = document.getElementById("root");
 if (!rootElement) {
-  throw new Error("Failed to find the root element");
+    throw new Error("Failed to find the root element");
 }
 
 const root = createRoot(rootElement);
 
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+    <StrictMode>
+        <HashRouter> {/* Wrap App in HashRouter */}
+            <App />
+        </HashRouter>
+    </StrictMode>
 );
